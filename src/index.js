c4501db59960188d2b8c3eeccc5ed0a154487833
Cogumelloo <cogumelloo99@gmail.com>
2025-01-21 13:24:12 -0300

    Teste 25

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ class heroi {
     constructor (nome, idade, tipo, ataque) {
         this.nome = nome
         this.idade = idade
-        this.tipo = tipo
-        this.ataque = ataque
+        this.tipo = tipo["Guerreiro", "Mago", "Ninja", "Monge"] // Guerreiro, Mago, Ninja e Monge
+        this.ataque = ataque // espada, magia, shuriken e artes marciais
     }
     if(tipo = "Guerreiro"){
-        ataque = "usou espada"
+        ataque = "espada"
     }
     atacar(){
-        console.log(`${this.nome} atacou ${ataque}`)
+        console.log(`${this.nome} atacou usando ${ataque}`)
     }
 }
 
-let meuHeroi = new heroi("Cogumelo", 25, "Guerreiro")
+let meuHeroi = new heroi("Cogumelo", 25, this.tipo[1])
